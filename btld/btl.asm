51ed945b479c2df5b20cd87ff10893f450a9d12d
Oussama Soudassi <wildric.auric@gmail.com>
2025-10-27 14:52:22 +0100

    moved

--- btld/btl.asm
+++ btld/btl.asm
@@ -5,28 +5,36 @@ mov ah, 0x0e ;tty
 
 ;mov al, 'h'
 ;int 0x10
-;mov al, 'E'
-;int 0x10
-;mov al, 'L'
-;int 0x10
-;int 0x10
 
-mov dx, [txtsize] 
-mov cx, 0 
+;main
 
-wrtl: 
-mov bx, txt
-add bx, cx 
-mov al, [bx]
-int 0x10 
-add cx, 1 
-cmp cx, dx 
-jne wrtl
+mov  bx, txt 
+call wrtl
 
 jmp $
 
+;functions
+;wrtstr: write line.
+;        args: 
+;             bx: contains address of the string
+wrtstr: 
+mov bx, bx 
+wrtlp:
+mov al, [bx]
+add bx, 1
+cmp al, 0
+je endwrtstr
+int 0x10
+jmp wrtstr
+endwrtstr:
+ret
+
+
+
+
+
 txt:
-    db "Hello World"
+    db "Hello BootSector",0
 txtsize:
     db 11
 times 510-($-$$) db 0

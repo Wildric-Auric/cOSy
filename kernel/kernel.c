6c433e04a25ed2143d043a312584f84bbbce7c5d
Oussama Soudassi <wildric.auric@gmail.com>
2025-10-30 02:35:24 +0100

    refactored print function, added port manipulation

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,12 +1,33 @@
-#define VIDEO_MEMORY  0xb8000
 
-char* txt = "Hello From C!";
-void main() {
+typedef unsigned char  uchar;
+typedef unsigned short ushort;
+
+#define VIDEO_MEMORY  0xb8000
+volatile int tty_cur = 0;
+int print_str(const char* str) {
     volatile char* vm = (volatile char*)VIDEO_MEMORY;
     char c;
     int  i = 0;
-    while (c = txt[i++]) {
-        *(vm+(i-1)*2)     = c;
-        *(vm+(i-1)*2+1)   = 0xA0;
+    while (c = str[i]) {
+        *(vm+tty_cur)     = c;
+        *(vm+tty_cur+1)    = 0xA0;
+        tty_cur += 2;
+        i++;
     }
+    return tty_cur;
+}
+
+uchar rd_port_byte(ushort port) {
+    uchar res; 
+    __asm__("in %%dx, %%al" : "=a" (res) : "d" (port));
+    return res;
+}
+
+void wrt_port_byte(uchar port, uchar data) {
+    __asm__("out %%al, %%dx" : : "a" (data), "d" (port));
+}
+
+void main() {
+    print_str("Hey there");
+    print_str("; From C!");
 }

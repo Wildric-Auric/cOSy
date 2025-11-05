7f5e80ca9656976862de9b47cd1029fcedfa35ef
Oussama Soudassi <wildric.auric@gmail.com>
2025-11-05 23:20:08 +0100

    added A20 line enabling code

--- btld/btl.asm
+++ btld/btl.asm
@@ -20,6 +20,10 @@ jmp $
 ; 0x7C00-0x7DFF--> bootsector
 ; 0x7E00-0x8600--> stage 2 (2048 bytes)
 ; 0x8600-0x7FFF--> kernel  
+;
+; when A20 is enabled:
+; 0xFFFF:0x10=0x100000 ---> Kernel
+; 0x00F00000--->stack
 init_boot_sector:
     mov [boot_drive], dl
     mov ax, 0x7C0
@@ -133,6 +137,76 @@ ldsk_err:
     call wrt_num16
     popa
     ret
+; check_a20: checks if line a20 is enabled.
+;       args:
+;           bx: address to check and to mutate
+;       ret:
+;           ax: 0 if line a20 is disabled 1 if enabled
+; http://wiki.osdev.org/A20_Line
+check_a20:
+    pushf
+    push ds
+    push es
+    push di
+    push si
+    cli
+    xor ax, ax
+    mov es, ax
+    not ax
+    mov ds, ax  
+    ; ds = 0xFFFF
+    ; es = 0x0000
+    mov di, bx  ; address to check
+    mov si, di
+    add si, 0x10
+    mov al, byte[es:di] ; 0x0     + 0xXX
+    push ax
+    mov al, byte[ds:si] ; 0xFFFF0 + 0xXX + 0x10
+    ; diff is 1 mb
+    push ax
+    mov byte[es:di], 0xDD
+    mov byte[ds:si], 0xFF
+    cmp byte[es:di], 0xFF
+    ; restore values
+    pop ax
+    mov [ds:si], al
+    pop ax
+    mov [es:di], al
+    ; xor ax, ax -->does not work it affects the ZF flag
+    mov ax, 0
+    je check_a20_end 
+    mov ax, 1
+    check_a20_end:
+        sti
+        pop si
+        pop di
+        pop es
+        pop ds
+        popf
+        ret
+
+enable_a20:
+    pusha
+    call check_a20
+    cmp ax, 0x1 
+    je enable_a20_end
+    ; fast gate
+    in al, 0x92
+    or al, 2
+    out 0x92, al
+
+    call check_a20
+    cmp ax, 0x1 
+    jne enable_a20_err 
+    ; TODO::add other ways to enable a20 line
+enable_a20_end:
+    popa
+    ret
+enable_a20_err:
+    wrts a20_err_str
+    popa
+    jmp $
+
 ; ------------------data------------------
 boot_drive: 
     db 0
@@ -148,6 +222,8 @@ ldsk_ok:
     db "Read disk success",0
 ldsk_err_str:
     db "Error loading disk: ",0
+a20_err_str:
+    db "Could not continue, A20 enabling impossible.",0
 ; https://wiki.osdev.org/GDT_Tutorial#What_to_Put_In_a_GDT
 ; https://wiki.osdev.org/Global_Descriptor_Table
 ; total base is 16, total limit 20 bit
@@ -158,23 +234,23 @@ gdt_beg:
 ; Code seg desc
 gdt_code:
     dw 0xFFFF     ;length 0-15  16
-    dw 0x0        ;base   16-31 16
-    db 0x0        ;base   32-39 8
+    dw 0x0        ;0x7E00
+    db 0x0        
     db 10011010b  ;access byte 40-47  8
     db 11001111b  ;flags+limit 48-55 4+4
     db 0x0        ;base    55-63 8
 ; Data Seg
 gdt_dat:
     dw 0xFFFF     
-    dw 0x0        
-    db 0x0        
+    dw 0x0 
+    db 0x0 
     db 10010010b  
     db 11001111b  
     db 0x0        
 gdt_end:
 gdt_desc:
     dw gdt_end - gdt_beg - 1    ;gdt size minus -1 
-    dd gdt_beg + 0x7E00        ;address 8 bytes
+    dd gdt_beg + 0x7E00         ;address 8 bytes
 code_seg equ gdt_code - gdt_beg
 data_seg equ gdt_dat  - gdt_beg
 
@@ -197,11 +273,16 @@ init_stage2:
     mov fs, ax
     mov gs, ax
     wrts stage2_str
+    ; enable A20 line
+    call enable_a20
     ; load kernel
     xor bx, bx
-    mov ax, 0x860 ; load kernel to 0x8600
+    ; loads the kernel at 1 MB - 0xFFFF:0x10 = 0x100000
+    ; mov ax, 0x860 ; load kernel to 0x8600
+    mov ax, 0xFFFF 
     mov es, ax 
-    mov al, 16
+    mov bx, 0x10
+    mov al, 16 
     mov cl, 5
     call ldsk
     ; switch to pm
@@ -227,10 +308,11 @@ init_pm:
     mov fs, ax
     mov gs, ax
     mov ebp, 0x7C00 ;stack must be divisible by 16
+    ; mov ebp, 0x00F00000
     mov esp, ebp
     mov ebx, pm_str + 0x7E00
     call wrt_str32
-    call 0x8600 - 0x7E00
+    call 0xF8200
     ret
 wrt_str32:
     pusha

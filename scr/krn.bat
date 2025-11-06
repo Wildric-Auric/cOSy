c2d76ec2eb9ef0fdb04cd88ddd8dc94554ef1f85
Oussama Soudassi <wildric.auric@gmail.com>
2025-11-06 05:42:21 +0100

    added configs

--- scr/krn.bat
+++ scr/krn.bat
@@ -8,7 +8,15 @@ wsl -e bash -ic ^
 mv *.o build/
 mkdir build\\entry
 mv build/kernel_entry.o build/entry/kernel_entry.o
+
+rem Link
+
 wsl -e bash -ic ^
-"i686-elf-ld  -Ttext 0x100000 build/entry/kernel_entry.o build/*.o --oformat binary -o build/kernel.bin"
+"i686-elf-ld  -T scr/linker.ld build/entry/kernel_entry.o build/*.o  -o build/kernel.elf"
+wsl -e bash -ic ^
+"i686-elf-objcopy -O binary build/kernel.elf build/kernel.bin"
+
+rem wsl -e bash -ic ^
+rem "i686-elf-ld  -Ttext 0x100000 build/entry/kernel_entry.o build/*.o --oformat binary -o build/kernel.bin"
 
 popd

960b3dc80a632fa88d279505a5d970f7c1dfc803
Oussama Soudassi <wildric.auric@gmail.com>
2025-11-09 22:06:04 +0100

    added utilities
    conversion from num to hex str
    length of string calculation

--- kernel/util.c
+++ kernel/util.c
@@ -13,3 +13,28 @@ char cnv_ascii_qwaz(char c) {
 boolean is_alpha_num(char c) {
     return (c >= 'a' && c <= 'z') || (c >= '0' && c <= '9');
 }
+
+void cnv_num_hex_str(ui32 n, ui32 d, char* out_buff) {
+    const char* lkup = "0123456789ABCDEF";
+    int r = 0;
+    int t = 0;
+    int l = 0;
+    out_buff[0] = '0'; out_buff[1] = 'x';
+    int i   = 2;
+    ui32 digit = 5;
+    while (d) {
+        digit      = mmin(n / d,15);       
+        out_buff[i++]  = lkup[digit];
+        n %= d;
+        d  = (~d)&(d>>4);
+    }
+    out_buff[i] = 0;
+}
+
+int str_len(const char* str) {
+    char c;
+    int  i = 0;
+    while (c = str[i++]);
+    return i;
+}
+
--- kernel/util.h
+++ kernel/util.h
@@ -4,5 +4,6 @@
 
 char    cnv_ascii_qwaz(char c);
 boolean is_alpha_num(char c);
+void    cnv_num_hex_str(ui32 n, ui32 d, char* out_buff);
 
 #endif

9e37301e095568afee77118799fa93f0b7daf117
Oussama Soudassi <wildric.auric@gmail.com>
2025-11-09 22:04:40 +0100

    added flag for bochs debugger

--- scr/run_bochs.bat
+++ scr/run_bochs.bat
@@ -1,4 +1,13 @@
 @echo off
 pushd %~dp0..
-bochs -debugger -f scr/bochs.config
+
+rem Check if the first argument is -d or --d
+if /I "%1"=="-d" (
+    bochs -debugger -f scr/bochs.config
+) else if /I "%1"=="--debug" (
+    bochs -debugger -f scr/bochs.config
+) else (
+    bochs -f scr/bochs.config
+)
+
 popd
